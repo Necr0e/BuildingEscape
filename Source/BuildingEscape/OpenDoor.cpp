a67a7b653be30d6fc991449b2aaefeb967ce396c
Otto Necro Seitamaa <necrotto@gmail.com>
2018-12-17 03:14:29 +0200

    End of day commit. -Player and starting room - Door Trigger -Start of grabbing functionality

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActorThatOpensDoor = GetWorld()->GetFirstPlayerController()->GetPawn();
+	DoorOwner = GetOwner();
+	
+}
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (PressurePlate->IsOverlappingActor(ActorThatOpensDoor))
+	{
+		
+		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetRealTimeSeconds();
+	}
+
+	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay)
+	{
+		CloseDoor();
+	}
+}
+
+void UOpenDoor::OpenDoor()
+{
+	DoorOwner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+}
+
+void UOpenDoor::CloseDoor()
+{
+	DoorOwner->SetActorRotation(FRotator(0.0f, 0.0f, 0.0f));
+}
+
+
+
